2b176089136f85b4123dc4ec2e7c5e91f961559d
Brynley Llewellyn-Roux <blynblack@gmail.com>
2022-07-08 12:40:32 +1000

    Re-implemented piece moving again, with some checks

--- src/board.rs
+++ src/board.rs
@@ -35,6 +35,16 @@ impl Board {
     }
 
     pub fn move_piece(&mut self, old_pos: &Position, new_pos: &Position) -> Result<(), &str> {
+        let old_rank = match self.layout.get_mut(old_pos.y) {
+            Some(rank) => rank,
+            None => return Err("Out of bounds access!"),
+        };
+        let old_square = match old_rank.get_mut(old_pos.x) {
+            Some(square) => square,
+            None => return Err("Out of bounds access!"),
+        };
+        let moved_piece = std::mem::replace(old_square, Square::Empty);
+        self.layout[new_pos.y][new_pos.x] = moved_piece;
         Ok(())
     }
 }
