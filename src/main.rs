f30e7b97a7eecec2853516349563240ca27c219c
brynblack <49110391+brynblack@users.noreply.github.com>
2022-07-09 12:49:04 +1000

    Simplified error handling

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,8 @@ use chess_backend::board::{Board, Position};
 fn main() {
     let mut board = Board::default();
 
-    match board.move_piece(&Position { x: 0, y: 1 }, &Position { x: 0, y: 2 }) {
-        Ok(_) => (),
-        Err(err) => eprintln!("{}", err),
+    if let Err(err) = board.move_piece(&Position { x: 0, y: 1 }, &Position { x: 0, y: 2 }) {
+        eprintln!("{}", err);
     }
 
     for rank in board.get_layout() {
